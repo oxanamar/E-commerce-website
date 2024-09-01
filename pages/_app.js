cebb48f7ef5cdc1c1b5946a1580970cc3029cba3
oxanadev <shana_bj@hotmail.com>
2024-09-01 21:34:25 +0500

    Redux toolkit setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
 import "@/styles/globals.scss";
+import { Provider } from "react-redux";
+import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [mounted, setMounted] = useState(false);
+  const [persistor, setPersistor] = useState(null);
+
+  useEffect(() => {
+    setMounted(true);
+
+    if (typeof window !== "undefined") {
+      const pStore = persistStore(store);
+      setPersistor(pStore);
+    }
+  }, []);
+
+  if (!mounted || !persistor) return null;
+
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
---
+++ pages/store/index.js
@@ -0,0 +1,41 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import cart from "./cartSlice";
+
+// Conditional import for storage
+const createNoopStorage = () => ({
+  getItem(_key) {
+    return Promise.resolve(null);
+  },
+  setItem(_key, value) {
+    return Promise.resolve(value);
+  },
+  removeItem(_key) {
+    return Promise.resolve();
+  },
+});
+
+const persistConfig = {
+  key: "root",
+  storage: typeof window !== "undefined" ? storage : createNoopStorage(),
+};
+
+const reducers = combineReducers({
+  cart,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(thunk),
+});
+
+export default store;
