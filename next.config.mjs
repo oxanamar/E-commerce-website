a3863eecbadec262ebd3e2b4ef966966d49e4a3d
oxanadev <shana_bj@hotmail.com>
2024-09-02 18:09:10 +0500

    Making Global variables accessible

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), "styles")],
+    prependData: `@import "./base.scss";`,
+  },
 };
 
 export default nextConfig;
